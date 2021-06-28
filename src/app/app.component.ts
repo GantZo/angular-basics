74664ab9d8c547a89348223fea1cb95ba5bcbd20
GantZo <putrify@mail.ru>
2021-06-28 14:55:23 +0300

    2 way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,31 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Dynamic title';
-  number = 42;
-  arr = [1,2,3];
-  obj = {a:1, b:{c:2}};
-  // img = 'https://banner2.cleanpng.com/20180604/pol/kisspng-react-javascript-angularjs-ionic-atom-5b154be6709500.6532453515281223424611.jpg';
 
-  inputValue = '';
+  title = 'Initial'
 
-  constructor() {
-    // setTimeout(() => {
-    //   console.log('timeout is over')
-    //   this.img = 'https://image.pngaaa.com/363/4145363-middle.png';
-    // }, 5000)
-  }
-
-  onInput(event : KeyboardEvent) {
-    console.log('Event ', event)
-    this.inputValue = (<HTMLInputElement>event.target).value;
-  }
-
-  onClick() {
-    console.log('Click!')
-  }
-
-  onBlur(value: string) {
-    this.inputValue = value;
+  onInput(event: any) {
+    this.title  = event.target.value
   }
 }
