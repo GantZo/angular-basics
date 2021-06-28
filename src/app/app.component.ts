83265bd088f1bab5be824bf824f06cfbf89d6af3
GantZo <putrify@mail.ru>
2021-06-28 17:48:26 +0300

    ch2: transfer data from components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,4 +16,8 @@ export class AppComponent {
     {title: 'Post 1', text: 'Post body for Post Component', id: 1},
     {title: 'Post 2', text: 'Second post body! yeah', id: 2}
   ]
+
+  updatePosts(post: Post) {
+    this.posts.unshift(post)
+  }
 }
