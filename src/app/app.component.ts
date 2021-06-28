cdf0323cd809406097ff142b3d2a724370d54878
GantZo <putrify@mail.ru>
2021-06-28 17:27:33 +0300

    ch2: fill posts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
+export interface Post {
+  title: string
+  text: string
+  id?: number
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-
+  posts: Post[] = [
+    {title: 'Post 1', text: 'Post body for Post Component', id: 1},
+    {title: 'Post 2', text: 'Second post body! yeah', id: 2}
+  ]
 }
