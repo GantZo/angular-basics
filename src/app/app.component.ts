cd1bdad87fbd29ec6542055f25fb813c2dacf0da
GantZo <putrify@mail.ru>
2021-06-28 14:35:48 +0300

    Add: 1way data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-basic';
+  title = 'Dynamic title';
+  number = 42;
+  arr = [1,2,3];
+  obj = {a:1, b:{c:2}};
+  // img = 'https://banner2.cleanpng.com/20180604/pol/kisspng-react-javascript-angularjs-ionic-atom-5b154be6709500.6532453515281223424611.jpg';
+
+  inputValue = '';
+
+  constructor() {
+    // setTimeout(() => {
+    //   console.log('timeout is over')
+    //   this.img = 'https://image.pngaaa.com/363/4145363-middle.png';
+    // }, 5000)
+  }
+
+  onInput(event : KeyboardEvent) {
+    console.log('Event ', event)
+    this.inputValue = (<HTMLInputElement>event.target).value;
+  }
+
+  onClick() {
+    console.log('Click!')
+  }
+
+  onBlur(value: string) {
+    this.inputValue = value;
+  }
 }
