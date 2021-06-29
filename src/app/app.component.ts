e08872c2cbd41d466279aa9feea2dcf4c1a6ac75
GantZo <putrify@mail.ru>
2021-06-29 13:35:47 +0300

    ch3: directives, access to elements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,39 +1,14 @@
 import {Component, OnInit} from '@angular/core';
 
-export interface Post {
-  title: string
-  text: string
-  id?: number
-}
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
-  posts: Post[] = [
-    {title: 'Post 1', text: 'Post body for Post Component', id: 1},
-    {title: 'Post 2', text: 'Next post!', id: 2}
-  ]
+export class AppComponent implements OnInit {
 
   ngOnInit(): void {
-    // setTimeout(() => {
-    //   console.log('Timeout')
-    //   // this.posts[0].title = 'Changed!' // not effective on ChangeDetectionStrategy.OnPush strategy
-    //   this.posts[0] = {
-    //     title: 'Changed', text: 'Post body for Post Component', id: 1
-    //   }
-    // }, 5000)
-  }
 
-  updatePosts(post: Post) {
-    this.posts.unshift(post)
-  }
-
-  removePost(id: number) {
-    console.log('Id to remove = ', id)
-    this.posts = this.posts.filter(p => p.id !== id)
   }
 
 }
---
+++ src/app/directives/style.directive.ts
@@ -0,0 +1,13 @@
+import {Directive, ElementRef, Renderer2} from "@angular/core";
+
+@Directive({
+  selector: "[appStyle]"
+})
+export class StyleDirective {
+
+  constructor(private el: ElementRef, private renderer: Renderer2) {
+    renderer.setStyle(el.nativeElement, 'color', 'blue')
+    // el.nativeElement.style.color = 'red'
+  }
+
+}
