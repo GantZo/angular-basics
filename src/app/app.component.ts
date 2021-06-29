b92f9d4090f55cd5c1f33fc0d128de4aebd40bc3
GantZo <putrify@mail.ru>
2021-06-29 15:12:52 +0300

    ch4: pipe number

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,12 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
 
-  isVisible: boolean = true
-
-  ngOnInit(): void {
-
-  }
+  e: number = Math.E
 
 }
