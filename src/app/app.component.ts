a114f7767002a4340230b9f55bb580f01262b7a8
GantZo <putrify@mail.ru>
2021-06-29 16:59:07 +0300

    ch4: pipe common filter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,25 @@
 import {Component} from '@angular/core';
 
+export interface Post {
+  title: string,
+  text: string
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  search: string = ''
+  searchField: string = 'title'
+
+  posts: Post[] = [
+    {title: 'Beer', text: 'The best beer in the world'},
+    {title: 'Bread', text: 'Bread is all of the world'},
+    {title: 'JavaScript', text: 'The worst language in the world'},
+  ]
 
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import {FormsModule} from "@angular/forms";
 import {MultByPipe} from "./pipe/mult-by.pipe";
-import { ExMarksPipe } from './pipe/ex-marks.pipe';
+import {ExMarksPipe} from './pipe/ex-marks.pipe';
+import {FilterPipe} from './pipe/filter.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     MultByPipe,
-    ExMarksPipe
+    ExMarksPipe,
+    FilterPipe
   ],
   imports: [
     BrowserModule,
