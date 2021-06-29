6adf3eb1a8e7ab6a19bbaf4d711b9a8af49091dc
GantZo <putrify@mail.ru>
2021-06-29 14:02:50 +0300

    ch3: directives setup params

--- src/app/directives/style.directive.ts
+++ src/app/directives/style.directive.ts
@@ -1,9 +1,13 @@
-import {Directive, ElementRef, HostListener, Renderer2} from "@angular/core";
+import {Directive, ElementRef, HostListener, Input, Renderer2} from "@angular/core";
 
 @Directive({
   selector: "[appStyle]"
 })
 export class StyleDirective {
+
+  @Input('appStyle') color: string = 'blue'
+  @Input() fontWeight = 'normal'
+  @Input() dStyles!: {border?: string, fontWeight?: string, borderRadius?: string}
 
   constructor(private el: ElementRef, private renderer: Renderer2) {
   }
@@ -16,12 +20,19 @@ export class StyleDirective {
 
   @HostListener('mouseenter')
   onMouseEnter() {
-    this.renderer.setStyle(this.el.nativeElement, 'color', 'blue')
+    this.renderer.setStyle(this.el.nativeElement, 'color', this.color)
+    // this.renderer.setStyle(this.el.nativeElement, 'fontWeight', this.fontWeight)
+    this.renderer.setStyle(this.el.nativeElement, 'fontWeight', this.dStyles.fontWeight)
+    this.renderer.setStyle(this.el.nativeElement, 'border', this.dStyles.border)
+    this.renderer.setStyle(this.el.nativeElement, 'borderRadius', this.dStyles.borderRadius)
   }
 
   @HostListener('mouseleave')
   onMouseLeave() {
     this.renderer.setStyle(this.el.nativeElement, 'color', null)
+    this.renderer.setStyle(this.el.nativeElement, 'fontWeight', null)
+    this.renderer.setStyle(this.el.nativeElement, 'border', null)
+    this.renderer.setStyle(this.el.nativeElement, 'borderRadius', null)
   }
 
 }
