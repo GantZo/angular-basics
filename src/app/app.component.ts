d0315e00179769ae0119b5d5b52bfc4e3023315a
GantZo <putrify@mail.ru>
2021-07-01 18:54:40 +0300

    ch7: http:get

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+export interface ToDo {
+  completed: boolean
+  title: string
+  id?: number
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  todos: ToDo[] = []
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit() {
+    this.http.get<ToDo[]>('https://jsonplaceholder.typicode.com/todos?_limit=2')
+      .subscribe(response => {
+        console.log('Response: ', response)
+        this.todos = response
+      })
+  }
 
 }
