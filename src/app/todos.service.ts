afbee6243f9857c76b08aaf8be84d25bf2299bb6
GantZo <putrify@mail.ru>
2021-07-04 20:07:06 +0300

    ch7: http observe params

--- src/app/todos.service.ts
+++ src/app/todos.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from "@angular/core";
-import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
+import {HttpClient, HttpEventType, HttpHeaders, HttpParams} from "@angular/common/http";
 import {Observable, throwError} from "rxjs";
-import {catchError, delay} from "rxjs/operators";
+import {catchError, delay, map, tap} from "rxjs/operators";
 
 export interface ToDo {
   completed: boolean
@@ -27,12 +27,15 @@ export class TodosService {
     let params = new HttpParams()
     params = params.append('_limit', '4')
     params = params.append('custom', 'any')
-    // return this.http.get<ToDo[]>('https://jsonplaceholder.typicode.com/todos?_limit=2')
     return this.http.get<ToDo[]>('https://jsonplaceholder.typicode.com/todos', {
-      // params: new HttpParams().set('_limit', '3')
-      params
+      params,
+      observe: 'response'
     })
       .pipe(
+        map((response: any) => {
+          console.log(response)
+          return response.body
+        }),
         delay(500),
         catchError(err => {
           console.log('Error: ', err.message)
@@ -41,13 +44,26 @@ export class TodosService {
       )
   }
 
-  removeTodo(id: any): Observable<void> {
-    return this.http.delete<void>(`https://jsonplaceholder.typicode.com/todos/${id}`)
+  removeTodo(id: any): Observable<any> {
+    return this.http.delete<void>(`https://jsonplaceholder.typicode.com/todos/${id}`, {
+      observe: "events"
+    }).pipe(
+      tap(event => {
+        if (event.type === HttpEventType.Sent) {
+          console.log('Sent', event)
+        }
+        if (event.type == HttpEventType.Response) {
+          console.log('Response', event)
+        }
+      })
+    )
   }
 
   completeTodo(id: any): Observable<ToDo> {
     return this.http.put<ToDo>(`https://jsonplaceholder.typicode.com/todos/${id}`, {
       completed: true
+    }, {
+      responseType: 'json'
     })
   }
 
