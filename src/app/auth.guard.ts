aad45470d8e0f71297a6bb523801b9272cd785e5
GantZo <putrify@mail.ru>
2021-07-05 15:15:24 +0300

    ch8: routing secure

---
+++ src/app/auth.guard.ts
@@ -0,0 +1,34 @@
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from "@angular/router";
+import {Observable} from "rxjs";
+import {Injectable} from "@angular/core";
+import {AuthService} from "./auth.service";
+
+@Injectable({providedIn: 'root'})
+export class AuthGuard implements CanActivate {
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {
+  }
+
+  // canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+  //   return undefined;
+  // }
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> | Promise<boolean> | boolean {
+
+    return this.authService.isAuthenticated().then(isAuth => {
+      if (isAuth) {
+        return true
+      } else {
+        return  this.router.navigate(['/'], {queryParams: {
+          auth: false
+          }})
+      }
+    })
+  }
+
+}
