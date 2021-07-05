2a9f3bae569fddec76c1cf27516c60a374da87bc
GantZo <putrify@mail.ru>
2021-07-05 14:23:57 +0300

    ch8: routing redirect on **

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import {PostsComponent} from './posts/posts.component'
 import {AboutExtraComponent} from './about-extra/about-extra.component'
 import {AppRoutingModule} from "./app-routing.module";
 import {PostComponent} from "./post/post.component";
+import { ErrorPageComponent } from './error-page/error-page.component';
 
 @NgModule({
   declarations: [
@@ -18,6 +19,7 @@ import {PostComponent} from "./post/post.component";
     PostsComponent,
     PostComponent,
     AboutExtraComponent,
+    ErrorPageComponent,
   ],
   imports: [
     BrowserModule,
