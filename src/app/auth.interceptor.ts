952ac7e083893d807db5837b48209e6d8444e8be
GantZo <putrify@mail.ru>
2021-07-05 11:48:05 +0300

    ch7: http interceptors modification request

--- src/app/auth.interceptor.ts
+++ src/app/auth.interceptor.ts
@@ -1,10 +1,21 @@
-import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
+import {HttpEvent, HttpEventType, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
 import {Observable} from "rxjs";
+import {tap} from "rxjs/operators";
 
 export class AuthInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     console.log('Intercept request:', req)
-    return next.handle(req)
+    const clone = req.clone({
+      headers: req.headers.append('AUTH', 'SOME RANDOM TOKEN')
+    })
+
+    return next.handle(clone).pipe(
+      tap(event => {
+        if (event.type === HttpEventType.Response) {
+          console.log('Interceptor response', event)
+        }
+      })
+    )
   }
 
 }
