2069a34e05d94c6c5b88e988a773bfebe3f58fe9
GantZo <putrify@mail.ru>
2021-07-06 15:13:47 +0300

    ch10: meta tags

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component, ComponentFactoryResolver, ViewChild} from '@angular/core'
 import {ModalComponent} from "./modal/modal.component";
 import {RefDirective} from "./ref.directive";
+import {Meta, Title} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-root',
@@ -12,7 +13,16 @@ export class AppComponent {
   @ViewChild(RefDirective)
   refDir!: RefDirective;
 
-  constructor(private resolver: ComponentFactoryResolver) {
+  constructor(
+    private resolver: ComponentFactoryResolver,
+    private title: Title,
+    private meta: Meta
+  ) {
+    title.setTitle('App Component Page')
+    meta.addTags([
+      {name: 'keywords', content: 'angular,google,yandex'},
+      {name: 'description', content: 'Angular basics application'}
+    ])
   }
 
   showModal() {
