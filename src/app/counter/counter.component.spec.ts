48afd7ea22665aea54c0776566508ee451a9e477
GantZo <putrify@mail.ru>
2021-07-06 20:44:49 +0300

    ch12: unit-tests test Form controls

--- src/app/counter/counter.component.spec.ts
+++ src/app/counter/counter.component.spec.ts
@@ -1,11 +1,12 @@
 import {CounterComponent} from "./counter.component";
+import {FormBuilder} from "@angular/forms";
 
 describe('CounterComponent', () => {
 
   let component: CounterComponent
 
   beforeEach(() => {
-    component = new CounterComponent()
+    component = new CounterComponent(new FormBuilder())
   })
 
   it('should increment counter by 1', () => {
@@ -26,6 +27,17 @@ describe('CounterComponent', () => {
 
     component.increment()
     expect(result).toBe(1)
-  });
+  })
+
+  it('should create form with two controls', () => {
+    expect(component.form.contains('login')).toBeTruthy()
+    expect(component.form.contains('password')).toBeTruthy()
+  })
+
+  it('should mark login as invalid if empty value', () => {
+    const control = component.form.controls['login']
+    control.setValue('')
+    expect(control.valid).toBeFalsy()
+  })
 
 })
