d00af46174c8c37fc89e9cb2b83a561e06ddbffb
GantZo <putrify@mail.ru>
2021-07-07 16:31:14 +0300

    ch13: integrate-tests configuration

--- src/app/counter/counter.component.spec.ts
+++ src/app/counter/counter.component.spec.ts
@@ -1,10 +1,20 @@
 import {CounterComponent} from "./counter.component";
+import {ComponentFixture, TestBed} from "@angular/core/testing";
 
 describe('CounterComponent', () => {
   let component: CounterComponent
+  let fixture: ComponentFixture<CounterComponent>
 
   beforeEach(() => {
-    component = new CounterComponent()
+    TestBed.configureTestingModule({
+      declarations: [CounterComponent]
+    })
+    fixture = TestBed.createComponent(CounterComponent)
+    component = fixture.componentInstance
   })
+
+  it('should be defined', function () {
+    expect(component).toBeDefined()
+  });
 
 })
