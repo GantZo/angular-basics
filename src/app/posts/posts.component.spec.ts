22d78c021b36f7340bd366f82354d4617eede30c
GantZo <putrify@mail.ru>
2021-07-07 14:48:09 +0300

    ch12: unit-test service add element

--- src/app/posts/posts.component.spec.ts
+++ src/app/posts/posts.component.spec.ts
@@ -1,6 +1,6 @@
 import {PostsComponent} from "./posts.component";
 import {PostsService} from "./posts.service";
-import {EMPTY, of} from "rxjs";
+import {EMPTY, of, throwError} from "rxjs";
 
 describe('PostsComponent', () => {
   let component: PostsComponent
@@ -24,6 +24,22 @@ describe('PostsComponent', () => {
     spyOn(service, 'fetch').and.returnValue(of(posts))
     component.ngOnInit()
     expect(component.posts.length).toBe(posts.length)
+  })
+
+  it('should add new post into list', () => {
+    const post = {title: 'test'}
+    const spy = spyOn(service, 'create').and.returnValue(of(post))
+    component.add(post.title)
+
+    expect(spy).toHaveBeenCalled()
+    expect(component.posts).toContain(post)
+  })
+
+  it('should set error message if something wrong', () => {
+    const error = 'error message'
+    spyOn(service, "create").and.returnValue(throwError(error))
+    component.add('post title')
+    expect(component.message).toBe(error)
   })
 
 })
